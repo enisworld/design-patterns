4e05b2d51f46218986a12fa630737b08078655e5
Enis Amuk <enis.amuk@gmail.com>
2021-07-14 10:53:37 +0300

    Update ObjectInfoBuilderDemo.java

--- creational/builder/ObjectInfoBuilderDemo.java
+++ creational/builder/ObjectInfoBuilderDemo.java
@@ -5,34 +5,34 @@ public class EstateAgent {
 		System.out.println("Started");
 		ObjectInfoBuilder objectInfoBuilder = new ObjectInfoBuilder();
 		ObjectInfo objectInfo = objectInfoBuilder.setObjectId("")
-														.setObjectName(""))
-														.setObjectType("")
-														.setObjectColor("")
-														.setX(0.0)
-														.setY(0.0)
-														.setZ(0.0)
-														.setWidth(0.0)
-														.setHeight(0.0)
-														.setDepth(0.0)
-														.setAngle(0.0)
-														.build();
+							.setObjectName(""))
+							.setObjectType("")
+							.setObjectColor("")
+							.setX(0.0)
+							.setY(0.0)
+							.setZ(0.0)
+							.setWidth(0.0)
+							.setHeight(0.0)
+							.setDepth(0.0)
+							.setAngle(0.0)
+							.build();
     
     
     		ObjectInfo objectInfo2 = objectInfoBuilder.setObjectId("")
-														.setObjectName(""))
-														.setObjectType("")
-														.setObjectColor("")
-														.setAngle(0.0)
-														.build();
+							.setObjectName(""))
+							.setObjectType("")
+							.setObjectColor("")
+							.setAngle(0.0)
+							.build();
     
    		ObjectInfo objectInfo3 = objectInfoBuilder.setX(0.0)
-														.setY(0.0)
-														.setZ(0.0)
-														.setWidth(0.0)
-														.setHeight(0.0)
-														.setDepth(0.0)
-														.setAngle(0.0)
-														.build();
+							.setY(0.0)
+							.setZ(0.0)
+							.setWidth(0.0)
+							.setHeight(0.0)
+							.setDepth(0.0)
+							.setAngle(0.0)
+							.build();
     
 
 		System.out.println(objectInfo.print());
