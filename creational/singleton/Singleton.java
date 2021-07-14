b1570a37fd24cc6bbe3033cf165a9010f47f2726
Enis Amuk <enis.amuk@gmail.com>
2021-07-14 11:52:56 +0300

    Update Singleton.java

--- creational/singleton/Singleton.java
+++ creational/singleton/Singleton.java
@@ -1,1 +1,62 @@
-
+// Singleton design pattern
+class Singleton
+{
+    private static Singleton obj;
+ 
+    // private constructor to force use of
+    // getInstance() to create Singleton object
+    private Singleton() {}
+  
+  
+ /************************************************************************/ 
+  /************************************************************************/
+/************************************************************************/    
+  ///Classical Java implementation of singleton
+    public static Singleton getInstance1()
+    {
+        if (obj==null)
+            obj = new Singleton();
+        return obj;
+    }
+  
+  /************************************************************************/
+ /************************************************************************/ 
+  /************************************************************************/
+  // Thread Synchronized Java implementation 
+  // Only one thread can execute this at a time
+    public static synchronized Singleton getInstance2()
+    {
+        if (obj==null)
+            obj = new Singleton();
+        return obj;
+    }
+  
+ /************************************************************************/
+ /************************************************************************/
+ /************************************************************************/
+  // Double Checked Locking
+  //Thread safe 
+  
+  public static Singleton getInstance3()
+    {
+        if (obj == null)
+        {
+            // To make thread safe
+            synchronized (Singleton.class)
+            {
+                // check again as multiple threads
+                // can reach above step
+                if (obj==null)
+                    obj = new Singleton();
+            }
+        }
+        return obj;
+    }
+  
+  
+  /************************************************************************/
+ /************************************************************************/
+ /************************************************************************/
+  
+  
+}
