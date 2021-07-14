794b890a511ae6cb92798ef99d5d7cff23a9c6c0
Enis Amuk <enis.amuk@gmail.com>
2021-07-14 13:26:29 +0300

    Create SingletonDemo.java

---
+++ creational/singleton/SingletonDemo.java
@@ -0,0 +1,11 @@
+public class SingletonDemo {
+
+	public static void main(String [] args) {
+    Singleton  singletonInstance = Singleton.getInstace();
+    Singleton  singletonInstance2 = Singleton.getInstace2();
+    Singleton  singletonInstance3 = Singleton.getInstace3();
+    
+  }
+  
+  
+}
